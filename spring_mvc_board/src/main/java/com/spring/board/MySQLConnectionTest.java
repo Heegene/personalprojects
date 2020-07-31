253124b23e4c1558b394fc1076607bbf86acd4e8
Heegene <dev.heejin@gmail.com>
2020-07-31 14:04:49 +0900

    200731 Test: Connection test

---
+++ spring_mvc_board/src/main/java/com/spring/board/MySQLConnectionTest.java
@@ -0,0 +1,57 @@
+package com.spring.board;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.*;
+
+
+public class MySQLConnectionTest {
+	static final String DRIVER = "com.mysql.jdbc.Driver";
+	static final String URL = "jdbc:mysql://localhost:3306/board?useSSL=false";
+	static final String USERNAME = "connectuser";
+	static final String PASSWORD = "connect123!@#";
+	
+	@Test
+	public void getMySQLConnectionTest() throws SQLException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			stmt = conn.createStatement();
+			
+			String sql = "SELECT subject, content, writer FROM board";
+			
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				System.out.println("제목은 " + rs.getString("subject") + "입니다. ");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (!conn.isClosed()) {
+				conn.close();
+			}
+			if (!stmt.isClosed()) {
+				stmt.close();
+			}
+			if (!rs.isClosed()) {
+				rs.close();
+			}
+		 	
+			
+		}
+	}
+	
+	
+}
