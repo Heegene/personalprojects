476b932e4e61de87a934f1de493853bc73c13a4f
Heegene <dev.heejin@gmail.com>
2020-08-05 15:42:36 +0900

    200805 TEST: viewresolver test in progress

--- spring_blog/src/main/java/com/heegene/web/board/controller/BoardController.java
+++ spring_blog/src/main/java/com/heegene/web/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.heegene.web.board.controller;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import com.heegene.web.board.service.BoardService;
 @RequestMapping(value = "/board")
 public class BoardController {
 	
-	@Inject
+	@Autowired
 	private BoardService boardService;
 	
 	@RequestMapping(value = "/getBoardList", method = RequestMethod.GET)
