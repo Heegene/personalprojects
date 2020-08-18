81d92227c038f038a63162132224ddd99e4afa38
Heegene <dev.heejin@gmail.com>
2020-08-18 14:10:41 +0900

    200818 FEAT: RUD update

--- springboot_blog/src/main/java/com/heegene/blog/service/BoardService.java
+++ springboot_blog/src/main/java/com/heegene/blog/service/BoardService.java
@@ -2,6 +2,7 @@ package com.heegene.blog.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -37,12 +38,37 @@ public class BoardService {
 					.title(boardEntity.getTitle())
 					.content(boardEntity.getContent())
 					.writer(boardEntity.getWriter())
-					.createdDate(boardEntity.getCreateDate())
+					.createdDate(boardEntity.getCreatedDate())
 					.build();
 			
 			boardDtoList.add(boardDto);
 		}
 		return boardDtoList;
+	}
+	
+	@Transactional
+	public BoardDto getPost(Long id) {
+		Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+		// findbyid = PK 값을 where 조건으로 하여 데이터를 가져오기 위한 메서드로 JPA repository 인터페이스에 정의되어 있음
+		// 반환 값은 Optional 타입으로, entity 를 빼 오기 위해서는 boardEntityWrapper.get 이런식으로 get() 메서드 사용
+		
+		BoardEntity boardEntity = boardEntityWrapper.get();
+		
+		BoardDto boardDto = BoardDto.builder()
+				.id(boardEntity.getId())
+				.title(boardEntity.getTitle())
+				.content(boardEntity.getContent())
+				.writer(boardEntity.getWriter())
+				.createdDate(boardEntity.getCreatedDate())
+				.build();
+		
+		return boardDto;
+	}
+	
+	@Transactional
+	public void deletePost(Long id) {
+		boardRepository.deleteById(id);
+		// pk 값을 where 조건으로 하여 데이터를 삭제하기 위한 method로, JPArepository 인터페이스에 정의되어 있음 
 	}
 	
 }
