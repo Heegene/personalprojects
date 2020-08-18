527421a9d95d7fe6899e305b68849442b43056fe
Heegene <dev.heejin@gmail.com>
2020-08-18 12:26:53 +0900

    200818 FEAT: list function update

--- springboot_blog/src/main/java/com/heegene/blog/service/BoardService.java
+++ springboot_blog/src/main/java/com/heegene/blog/service/BoardService.java
@@ -1,9 +1,13 @@
 package com.heegene.blog.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.heegene.blog.domain.entity.BoardEntity;
 import com.heegene.blog.domain.repository.BoardRepository;
 import com.heegene.blog.dto.BoardDto;
 
@@ -20,5 +24,25 @@ public class BoardService {
 		// save는 JPA repository에 정의된 method로, DB에 insert, update를 담당하고 매개변수로 entity 전달
 	}
 	
+	@Transactional
+	public List<BoardDto> getBoardList() {
+		List<BoardEntity> boardEntities = boardRepository.findAll();
+		List<BoardDto> boardDtoList = new ArrayList<>();
+		// controller와 service 간의 데이터 전달은 dto 객체로 하기 위해
+		// repository에서 가져온 entity를 반복문을 통해 dto로 변환 
+		
+		for (BoardEntity boardEntity : boardEntities) {
+			BoardDto boardDto = BoardDto.builder()
+					.id(boardEntity.getId())
+					.title(boardEntity.getTitle())
+					.content(boardEntity.getContent())
+					.writer(boardEntity.getWriter())
+					.createdDate(boardEntity.getCreateDate())
+					.build();
+			
+			boardDtoList.add(boardDto);
+		}
+		return boardDtoList;
+	}
 	
 }
